acbd072da3c50b67d73ae5b13a320c04d6dec08b
GodlyVert <42441281+GodlyVert@users.noreply.github.com>
2021-03-20 14:59:01 +0200

    First load

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        adaptiveHeight:true,
+        slidesToShow: 3,
+        infinite: true,
+        mobileFirst: true
+    });
+});
